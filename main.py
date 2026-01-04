0d8093357bf095d8968ad2ba0b3fa28ddb747c7f
egramsdoescode <ethandoescode@gmail.com>
2026-01-04 08:56:44 -0600

    Maximize game framerate at 60 FPS

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -21,6 +24,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
